71ab4d5c164e860a8564390887cf6f616f0a78a0
Tobias Rodriguez Viau <rodriguezviautobias@gmail.com>
2025-12-13 20:34:16 -0300

    feat: add documentation

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,12 +5,32 @@ import (
 	"log"
 	"os"
 
+	_ "github.com/TobiasRV/challenge-fs-senior/docs"
 	"github.com/TobiasRV/challenge-fs-senior/internals/db"
 	"github.com/TobiasRV/challenge-fs-senior/internals/handlers"
 	"github.com/TobiasRV/challenge-fs-senior/internals/repository"
 	"github.com/TobiasRV/challenge-fs-senior/internals/router"
 	"github.com/joho/godotenv"
 )
+
+// @title Challenge FS Senior API
+// @version 1.0
+// @description API for managing teams, projects, tasks, and users
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.email support@example.com
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
 
 func main() {
 	godotenv.Load()
